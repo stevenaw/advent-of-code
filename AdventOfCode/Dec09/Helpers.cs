50df2ed849ba4bf9eead68a1ebb5319dc12f69f9
stevenaw <sweerdenburg@gmail.com>
2020-12-09 20:04:49 -0500

    Day 9 done

---
+++ AdventOfCode/Dec09/Helpers.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace AdventOfCode2020.Dec09
+{
+    static class Helpers
+    {
+        public static long FindFirstToBreakPattern(long[] numbers)
+        {
+            const int preambleSize = 25;
+
+            for (var i = preambleSize; i < numbers.Length; i++)
+            {
+                var valid = false;
+                for (var j = i - preambleSize; j < i && !valid; j++)
+                {
+                    for (var k = i - preambleSize; k < i && !valid; k++)
+                    {
+                        if (numbers[j] != numbers[k] && numbers[j] + numbers[k] == numbers[i])
+                        {
+                            valid = true;
+                        }
+                    }
+                }
+
+                if (!valid)
+                    return numbers[i];
+            }
+
+            return -1;
+        }
+        public static ArraySegment<long> FindContiguousSum(long[] numbers, long target)
+        {
+            for (var i = 0; i < numbers.Length; i++)
+            {
+                var sum = numbers[i];
+
+                var j = i + 1;
+                for (; j < numbers.Length && sum < target; j++)
+                    sum += numbers[j];
+
+                if (sum == target && j != i + 1)
+                    return new ArraySegment<long>(numbers, i, j - i);
+            }
+
+            return ArraySegment<long>.Empty;
+        }
+    }
+}
---
+++ AdventOfCode/Dec09/Puzzle1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2020.Dec09
+{
+    class Puzzle1 : AdventPuzzle
+    {
+        protected override long Solve(IEnumerable<string> lines)
+        {
+            var numbers = lines.Select(o => Int64.Parse(o)).ToArray();
+            var comboBreaker = Helpers.FindFirstToBreakPattern(numbers);
+
+            return comboBreaker;
+
+
+
+
+
+            //Span<int> circularBuffer = stackalloc int[preambleSize];
+
+            //// Build preamble
+            //var lineEnumerator = lines.GetEnumerator();
+            //for (var i = 0; i < preambleSize; i++)
+            //{
+            //    lineEnumerator.MoveNext();
+            //    circularBuffer[i] = int.Parse(lineEnumerator.Current);
+            //}
+
+            //var idx = 0;
+            //var invalidNumber = 0;
+            //while (lineEnumerator.MoveNext())
+            //{
+            //    var nextNumber = int.Parse(lineEnumerator.Current);
+            //    var valid = false;
+
+            //    for(var i = 0; i < circularBuffer.Length && !valid; i++)
+            //    {
+            //        for (var j = 0; j < circularBuffer.Length && !valid; j++)
+            //        {
+            //            if (circularBuffer[i] != circularBuffer[j] && circularBuffer[i] + circularBuffer[j] == nextNumber)
+            //            {
+            //                valid = true;
+            //            }
+            //        }
+            //    }
+
+            //    if (valid)
+            //    {
+            //        circularBuffer[idx] = nextNumber;
+            //        idx = (idx + 1) % preambleSize;
+            //    }
+            //    else
+            //    {
+            //        invalidNumber = nextNumber;
+            //        break;
+            //    }
+            //}
+
+            //return invalidNumber;
+        }
+    }
+}
