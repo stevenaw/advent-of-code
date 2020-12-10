2dc863189bad393a68872235f384b728bd58dfde
stevenaw <sweerdenburg@gmail.com>
2020-12-09 20:06:03 -0500

    Remove commented code

--- AdventOfCode/Dec09/Puzzle1.cs
+++ AdventOfCode/Dec09/Puzzle1.cs
@@ -12,52 +12,6 @@ namespace AdventOfCode2020.Dec09
             var comboBreaker = Helpers.FindFirstToBreakPattern(numbers);
 
             return comboBreaker;
-
-
-
-
-
-            //Span<int> circularBuffer = stackalloc int[preambleSize];
-
-            //// Build preamble
-            //var lineEnumerator = lines.GetEnumerator();
-            //for (var i = 0; i < preambleSize; i++)
-            //{
-            //    lineEnumerator.MoveNext();
-            //    circularBuffer[i] = int.Parse(lineEnumerator.Current);
-            //}
-
-            //var idx = 0;
-            //var invalidNumber = 0;
-            //while (lineEnumerator.MoveNext())
-            //{
-            //    var nextNumber = int.Parse(lineEnumerator.Current);
-            //    var valid = false;
-
-            //    for(var i = 0; i < circularBuffer.Length && !valid; i++)
-            //    {
-            //        for (var j = 0; j < circularBuffer.Length && !valid; j++)
-            //        {
-            //            if (circularBuffer[i] != circularBuffer[j] && circularBuffer[i] + circularBuffer[j] == nextNumber)
-            //            {
-            //                valid = true;
-            //            }
-            //        }
-            //    }
-
-            //    if (valid)
-            //    {
-            //        circularBuffer[idx] = nextNumber;
-            //        idx = (idx + 1) % preambleSize;
-            //    }
-            //    else
-            //    {
-            //        invalidNumber = nextNumber;
-            //        break;
-            //    }
-            //}
-
-            //return invalidNumber;
         }
     }
 }
