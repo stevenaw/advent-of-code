ca0b1ff20973debe1cd45342d9c9337fc8a4ef3b
stevenaw <sweerdenburg@gmail.com>
2021-12-05 22:07:55 -0500

    2015 1.2

--- AdventOfCode.Y2015/Dec01/Puzzle2.cs
+++ AdventOfCode.Y2015/Dec01/Puzzle2.cs
@@ -1,21 +1,24 @@
 ﻿namespace AdventOfCode.Y2015.Dec01
 {
-    internal class Puzzle1 : AdventPuzzle
+    internal class Puzzle2 : AdventPuzzle
     {
-        protected override long Solve(IEnumerable<string> fileContents)
+        protected override long Solve(IEnumerable<string> lines)
         {
             var level = 0L;
 
-            foreach(var line in fileContents)
+            foreach (var line in lines)
                 for (var i = 0; i < line.Length; i++)
                 {
                     if (line[i] == '(')
                         level++;
                     else if (line[i] == ')')
                         level--;
+
+                    if (level < 0)
+                        return i + 1;
                 }
 
-            return level;
+            return -1;
         }
     }
 }
