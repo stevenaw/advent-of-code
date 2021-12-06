0b5dc9761b29bc7804a6d90d58b1ef99ac62d10d
stevenaw <sweerdenburg@gmail.com>
2021-12-06 16:05:33 -0500

    2015 3.1

---
+++ AdventOfCode.Y2015/Dec03/Coordinate.cs
@@ -0,0 +1,17 @@
+﻿namespace AdventOfCode.Y2015.Dec03
+{
+    internal record Coordinate
+    {
+        public int X { get; init; }
+        public int Y { get; init; }
+
+        public Coordinate Translate(char move) => move switch
+        {
+            '<' => new Coordinate() { X = X - 1, Y = Y },
+            '>' => new Coordinate() { X = X + 1, Y = Y },
+            'v' => new Coordinate() { X = X, Y = Y - 1 },
+            '^' => new Coordinate() { X = X, Y = Y + 1 },
+            _ => throw new ArgumentOutOfRangeException(nameof(move))
+        };
+    }
+}
---
+++ AdventOfCode.Y2015/Dec03/Puzzle1.cs
@@ -0,0 +1,23 @@
+﻿namespace AdventOfCode.Y2015.Dec03
+{
+    internal class Puzzle1 : AdventPuzzle
+    {
+        protected override long Solve(IEnumerable<string> lines)
+        {
+            var line = lines.First();
+
+            var visits = new HashSet<Coordinate>();
+            var coord = new Coordinate();
+
+            visits.Add(coord);
+
+            foreach (var move in line)
+            {
+                coord = coord.Translate(move);
+                visits.Add(coord);
+            }
+
+            return visits.Count;
+        }
+    }
+}
