ad765c45125173932968260696019d9d08697363
stevenaw <sweerdenburg@gmail.com>
2021-12-28 20:50:54 -0500

    2015 8.1

---
+++ AdventOfCode.Y2015/Dec08/Puzzle1.cs
@@ -0,0 +1,42 @@
+﻿namespace AdventOfCode.Y2015.Dec08
+{
+    internal class Puzzle1 : AdventPuzzle
+    {
+        protected override long Solve(IEnumerable<string> lines)
+        {
+            var count = 0;
+
+            foreach(var line in lines)
+            {
+                var allChars = line.Length;
+                var displayableChars = CountDisplayableCharacters(line);
+                count += (allChars - displayableChars);
+            }
+
+            return count;
+        }
+
+        private static int CountDisplayableCharacters(ReadOnlySpan<char> line)
+        {
+            if (line[0] == '"')
+                line = line[1..];
+            if (line[^1] == '"')
+                line = line[0..^1];
+
+            var count = 0;
+            for (var i = 0; i < line.Length; i++)
+            {
+                if (line[i] == '\\')
+                {
+                    if (line[i + 1] == 'x')
+                        i += 3;
+                    else
+                        i++;
+                }
+                
+                count++;
+            }
+            return count;
+        }
+    }
+}
