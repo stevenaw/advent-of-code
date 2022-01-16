da6d236d977a2dd3dc3799856a8099b406aec9a1
stevenaw <sweerdenburg@gmail.com>
2022-01-15 21:23:29 -0500

    2015 11.1

---
+++ AdventOfCode.Y2015/Dec11/PasswordGenerator.cs
@@ -0,0 +1,71 @@
+﻿namespace AdventOfCode.Y2015.Dec11
+{
+    internal static class PasswordGenerator
+    {
+        public static string[] GenerateNextPasswords(string seed, int n)
+        {
+            var chars = seed.ToCharArray().AsSpan();
+            var result = new string[n];
+
+            for (var i = 0; i < n; i++)
+            {
+                do
+                {
+                    Increment(chars);
+                } while (!FollowsRule1(chars) || !FollowsRule2(chars) || !FollowsRule3(chars));
+
+                result[i] = chars.ToString()!;
+            }
+
+            return result;
+        }
+
+        private static bool FollowsRule1(ReadOnlySpan<char> chars)
+        {
+            for (var i = 0; i < chars.Length - 2; i++)
+                if (chars[i] + 1 == chars[i + 1] && chars[i + 1] + 1 == chars[i + 2])
+                    return true;
+            return false;
+        }
+
+        private static bool FollowsRule2(ReadOnlySpan<char> chars)
+        {
+            return chars.IndexOfAny('i', 'o', 'l') == -1;
+        }
+
+        private static bool FollowsRule3(ReadOnlySpan<char> chars)
+        {
+            var pairCount = 0;
+            for (var i = 0; i < chars.Length - 1; i++)
+            {
+                if (chars[i] == chars[i + 1])
+                {
+                    pairCount++;
+                    i++;
+                }
+            }
+            return pairCount > 1;
+        }
+
+        private static void Increment(Span<char> chars)
+        {
+            var idx = chars.Length - 1;
+            while (idx >= 0 && Increment(chars, idx))
+                idx--;
+        }
+
+        private static bool Increment(Span<char> chars, int i)
+        {
+            if (chars[i] == 'z')
+            {
+                chars[i] = 'a';
+                return true;
+            }
+            else
+            {
+                chars[i]++;
+                return false;
+            }
+        }
+    }
+}
---
+++ AdventOfCode.Y2015/Dec11/Puzzle1.cs
@@ -0,0 +1,27 @@
+﻿namespace AdventOfCode.Y2015.Dec11
+{
+    internal class Puzzle1 : AdventPuzzle
+    {
+        protected override long Solve(IEnumerable<string> lines)
+        {
+            var input = lines.First();
+
+            var result = PasswordGenerator.GenerateNextPasswords(input, 1);
+
+            Console.WriteLine(result[0]);
+
+            return EncodeAsLong(result[0]);
+        }
+
+        private static long EncodeAsLong(string s)
+        {
+            var result = 0L;
+            var idx = s.Length;
+
+            foreach(var c in s)
+                result |= ((byte)c) << (--idx*8);
+
+            return result;
+        }
+    }
+}
