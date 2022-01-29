730a422a257591de66fa765c55133b2b0f286a0e
stevenaw <sweerdenburg@gmail.com>
2022-01-29 13:23:22 -0500

    A bit of refactoring

---
+++ AdventOfCode.Y2015/Dec13/SeatingOptimizer.cs
@@ -0,0 +1,35 @@
+﻿namespace AdventOfCode.Y2015.Dec13
+{
+    internal static class SeatingOptimizer
+    {
+        public static int CalculateOptimalHappiness(Neighbour[] weights)
+        {
+            var distinctNames = weights.Select(o => o.PersonA)
+                .Union(weights.Select(o => o.PersonB))
+                .Distinct()
+                .ToArray();
+
+            var allCombinations = Combinatorial.GetPermutations(distinctNames);
+            var optimal = allCombinations.Select(o => CalculateSeatingHappiness(o, weights)).Max();
+
+            return optimal;
+        }
+
+        private static int CalculateSeatingHappiness(string[] seating, Neighbour[] weights)
+        {
+            var happiness = 0;
+            for (var i = 0; i < seating.Length - 1; i++)
+                happiness += CalculateHappiness(seating[i], seating[i + 1], weights);
+
+            happiness += CalculateHappiness(seating[seating.Length - 1], seating[0], weights);
+
+            return happiness;
+
+            static int CalculateHappiness(string a, string b, Neighbour[] weights)
+            {
+                var weight = weights.First(o => (o.PersonA == a && o.PersonB == b) || (o.PersonA == b && o.PersonB == a));
+                return weight.DeltaHappiness;
+            }
+        }
+    }
+}
---
+++ AdventOfCode/Combinatorial.cs
@@ -0,0 +1,53 @@
+﻿namespace AdventOfCode
+{
+    public static class Combinatorial
+    {
+        public static List<T[]> GetPermutations<T>(T[] seed) where T : class
+        {
+            var capacity = GetFactorial(seed.Length - 1);
+            var tally = new List<T[]>(capacity);
+            var rest = new Stack<T>(seed.Reverse());
+
+            // anchor by first item, rest relative to it
+            var buffer = new T[seed.Length];
+            buffer[0] = rest.Pop();
+
+            BuildIteration(buffer, rest, tally);
+
+            return tally;
+
+            static void BuildIteration(T?[] buffer, Stack<T> rest, List<T[]> tally)
+            {
+                var c = rest.Count;
+                if (c == 0)
+                    return;
+
+                var next = rest.Pop();
+
+                for (var i = 1; i < buffer.Length; i++)
+                {
+                    if (buffer[i] is null)
+                    {
+                        buffer[i] = next;
+
+                        BuildIteration(buffer, rest, tally);
+
+                        if (!rest.Any())
+                            tally.Add((T[])buffer.Clone());
+
+                        buffer[i] = null;
+                    }
+                }
+
+                rest.Push(next);
+            }
+        }
+
+        private static int GetFactorial(int n)
+        {
+            if (n <= 2)
+                return n;
+            return n * GetFactorial(n - 1);
+        }
+    }
+}
