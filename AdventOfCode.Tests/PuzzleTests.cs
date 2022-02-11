e70792826a77863d56b33f833669d3c2a9bcb0b7
stevenaw <sweerdenburg@gmail.com>
2022-02-11 11:21:20 -0500

    2016 3.2

--- AdventOfCode.Tests/PuzzleTests.cs
+++ AdventOfCode.Tests/PuzzleTests.cs
@@ -39,6 +39,7 @@ namespace AdventOfCode.Tests
         [TestCase(2016, 2, 1, ExpectedResult = 84452)]
         [TestCase(2016, 2, 2, ExpectedResult = 909460343)]
         [TestCase(2016, 3, 1, ExpectedResult = 1032)]
+        [TestCase(2016, 3, 2, ExpectedResult = 1838)]
 
         [TestCase(2020, 1, 1, ExpectedResult = 878724)]
         [TestCase(2020, 1, 2, ExpectedResult = 201251610)]
---
+++ AdventOfCode.Y2016/Dec03/Puzzle2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Y2016.Dec03
+{
+    internal class Puzzle2 : AdventPuzzle
+    {
+        protected override long Solve(IEnumerable<string> lines)
+        {
+            var items = lines.Select(x => x.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray()).ToArray();
+            var count = 0L;
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 2; j < items.Length; j+=3)
+                {
+                    var shape = (a: items[j][i], b: items[j-1][i], c: items[j-2][i]);
+
+                    if (shape.a + shape.b > shape.c
+                    && shape.c + shape.b > shape.a
+                    && shape.a + shape.c > shape.b)
+                        count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
