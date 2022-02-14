5b8b1718203ec21a0f6c45045decd671b9b21a83
stevenaw <sweerdenburg@gmail.com>
2022-02-14 16:30:56 -0500

    refactor

--- AdventOfCode.Y2016/Dec04/InputParser.cs
+++ AdventOfCode.Y2016/Dec04/InputParser.cs
@@ -6,14 +6,25 @@ namespace AdventOfCode.Y2016.Dec04
     {
         private static readonly Regex Parser = new Regex(@"^((\w+\-)+)(\d+)\[(\w{5})\]$", RegexOptions.Compiled | RegexOptions.IgnoreCase);
 
-        public static (string encryptedName, int sectorId, string checksum) Parse(string line)
+        public static InputRecord Parse(string line)
         {
             var matches = Parser.Match(line).Groups;
-            return (
+            return new InputRecord(
                 matches[1].Value.TrimEnd('-'),
                 int.Parse(matches[3].ValueSpan),
                 matches[4].Value
             );
+        }
+
+        public record struct InputRecord (string EncryptedName, int SectorId, string Checksum)
+        {
+            public bool IsValid()
+            {
+                var charCounts = EncryptedName.Where(e => e != '-').GroupBy(e => e).ToDictionary(e => e.Key, e => e.Count());
+                var top5 = charCounts.OrderByDescending(o => o.Value).ThenBy(o => o.Key).Take(5).Select(o => o.Key).ToArray();
+
+                return Checksum.AsSpan().SequenceEqual(top5);
+            }
         }
     }
 }
--- AdventOfCode.Y2016/Dec04/Puzzle1.cs
+++ AdventOfCode.Y2016/Dec04/Puzzle1.cs
@@ -8,20 +8,12 @@
 
             foreach (var line in lines)
             {
-                var (encryptedName, sectorId, checksum) = InputParser.Parse(line);
-                if (IsValid(encryptedName, checksum))
-                    sum += sectorId;
+                var item = InputParser.Parse(line);
+                if (item.IsValid())
+                    sum += item.SectorId;
             }
 
             return sum;
-        }
-
-        private static bool IsValid(string encryptedName, string checksum)
-        {
-            var charCounts = encryptedName.Where(e => e != '-').GroupBy(e => e).ToDictionary(e => e.Key, e => e.Count());
-            var top5 = charCounts.OrderByDescending(o => o.Value).ThenBy(o => o.Key).Take(5).Select(o => o.Key).ToArray();
-
-            return string.Equals(new string(top5), checksum);
         }
     }
 }
