60bf33ba0f22de05c5dfb37ddfa4f24d12409f1c
stevenaw <sweerdenburg@gmail.com>
2022-02-16 14:20:22 -0500

    2016 5.1

---
+++ AdventOfCode.Y2016/Dec05/Puzzle1.cs
@@ -0,0 +1,38 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace AdventOfCode.Y2016.Dec05
+{
+    internal class Puzzle1 : AdventPuzzle
+    {
+        protected override long Solve(IEnumerable<string> lines)
+        {
+            const int DigitCount = 8;
+
+            Span<char> password = stackalloc char[DigitCount];
+
+            var payloadPrefix = lines.First();
+            var suffixIdx = -1;
+
+            for (var i = 0; i < DigitCount; i++)
+            {
+                string hash;
+
+                do
+                {
+                    suffixIdx++;
+
+                    var payload = payloadPrefix + suffixIdx.ToString();
+                    var stringBytes = Encoding.Default.GetBytes(payload.ToString());
+                    var hashBytes = MD5.HashData(stringBytes);
+
+                    hash = Convert.ToHexString(hashBytes);
+                } while (!hash.StartsWith("00000"));
+
+                password[i] = hash[5];
+            }
+
+            return TypeEncoder.EncodeAsLong(password.ToString());
+        }
+    }
+}
