6df42c756ae5cadb2d3c33990b2cd3b758eb8f92
stevenaw <sweerdenburg@gmail.com>
2022-02-17 11:41:51 -0500

    2016 5.2

--- AdventOfCode.Tests/PuzzleTests.cs
+++ AdventOfCode.Tests/PuzzleTests.cs
@@ -43,6 +43,7 @@ namespace AdventOfCode.Tests
         [TestCase(2016, 4, 1, ExpectedResult = 278221)]
         [TestCase(2016, 4, 2, ExpectedResult = 267)]
         [TestCase(2016, 5, 1, ExpectedResult = 1987397493)]
+        [TestCase(2016, 5, 2, ExpectedResult = 1999783735)]
 
         [TestCase(2020, 1, 1, ExpectedResult = 878724)]
         [TestCase(2020, 1, 2, ExpectedResult = 201251610)]
---
+++ AdventOfCode.Y2016/Dec05/Puzzle2.cs
@@ -0,0 +1,30 @@
+﻿namespace AdventOfCode.Y2016.Dec05
+{
+    internal class Puzzle2 : AdventPuzzle
+    {
+        protected override long Solve(IEnumerable<string> lines)
+        {
+            Span<char> password = stackalloc char[8];
+
+            var payloadPrefix = lines.First();
+            var suffixIdx = 0;
+            var neededDigits = byte.MaxValue;
+
+            while (neededDigits != 0)
+            {
+                string hash;
+                while (!(hash = Hasher.HashAsHexString(payloadPrefix, suffixIdx++)).StartsWith("00000"))
+                { }
+
+                var pos = (int)(hash[5] - '0');
+                if ((neededDigits & (1UL << pos)) != 0)
+                {
+                    neededDigits &= (byte)~(1UL << pos);
+                    password[pos] = hash[6];
+                }
+            }
+
+            return TypeEncoder.EncodeAsLong(password);
+        }
+    }
+}
