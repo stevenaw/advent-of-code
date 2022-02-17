9496219d3b143dea27703897995d51e476f4dec9
stevenaw <sweerdenburg@gmail.com>
2022-02-17 06:30:34 -0500

    do it off the stack

---
+++ AdventOfCode.Y2016/Dec05/Hasher.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace AdventOfCode.Y2016.Dec05
+{
+    internal static class Hasher
+    {
+        public static string HashAsHexString(string prefix, int suffix)
+        {
+            Span<char> payload = stackalloc char[prefix.Length + 10];
+            prefix.CopyTo(payload.Slice(0, prefix.Length));
+            suffix.TryFormat(payload.Slice(prefix.Length), out var len);
+
+            var trimmedPayload = payload.Slice(0, prefix.Length + len);
+
+            Span<byte> payloadBytes = stackalloc byte[trimmedPayload.Length * sizeof(char)];
+            var size = Encoding.Default.GetBytes(trimmedPayload, payloadBytes);
+            payloadBytes = payloadBytes.Slice(0, size);
+
+            Span<byte> hashBytes = stackalloc byte[16];
+            MD5.HashData(payloadBytes, hashBytes);
+
+            return Convert.ToHexString(hashBytes);
+        }
+    }
+}
--- AdventOfCode.Y2016/Dec05/Puzzle1.cs
+++ AdventOfCode.Y2016/Dec05/Puzzle1.cs
@@ -12,22 +12,14 @@ namespace AdventOfCode.Y2016.Dec05
             Span<char> password = stackalloc char[DigitCount];
 
             var payloadPrefix = lines.First();
-            var suffixIdx = -1;
+            var suffixIdx = 0;
 
             for (var i = 0; i < DigitCount; i++)
             {
                 string hash;
 
-                do
-                {
-                    suffixIdx++;
-
-                    var payload = payloadPrefix + suffixIdx.ToString();
-                    var stringBytes = Encoding.Default.GetBytes(payload.ToString());
-                    var hashBytes = MD5.HashData(stringBytes);
-
-                    hash = Convert.ToHexString(hashBytes);
-                } while (!hash.StartsWith("00000"));
+                while (!(hash = Hasher.HashAsHexString(payloadPrefix, suffixIdx++)).StartsWith("00000"))
+                { }
 
                 password[i] = hash[5];
             }
