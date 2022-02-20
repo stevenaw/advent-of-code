3d1418366f7750b9d6a7042bec011ef2a71bfb22
stevenaw <sweerdenburg@gmail.com>
2022-02-20 11:19:52 -0500

    2016 7.2

--- AdventOfCode.Tests/PuzzleTests.cs
+++ AdventOfCode.Tests/PuzzleTests.cs
@@ -47,6 +47,7 @@ namespace AdventOfCode.Tests
         [TestCase(2016, 6, 1, ExpectedResult = 2105244271)]
         [TestCase(2016, 6, 2, ExpectedResult = 2104389479)]
         [TestCase(2016, 7, 1, ExpectedResult = 105)]
+        [TestCase(2016, 7, 2, ExpectedResult = 258)]
 
         [TestCase(2020, 1, 1, ExpectedResult = 878724)]
         [TestCase(2020, 1, 2, ExpectedResult = 201251610)]
---
+++ AdventOfCode.Y2016/Dec07/Puzzle2.cs
@@ -0,0 +1,40 @@
+﻿namespace AdventOfCode.Y2016.Dec07
+{
+    internal class Puzzle2 : AdventPuzzle
+    {
+        protected override long Solve(IEnumerable<string> lines)
+        {
+            return lines.Count(l => IsValid(l));
+        }
+
+        private static bool IsValid(ReadOnlySpan<char> line)
+        {
+            var tokens = Parser.Tokenize(line);
+
+            for (var i = 0; i < tokens.Length; i += 2)
+            {
+                foreach(var aba in EnumerateAbaPatterns(tokens[i]))
+                {
+                    var bab = string.Create(3, aba, (buffer, state) =>
+                    {
+                        buffer[2] = buffer[0] = state[1];
+                        buffer[1] = state[0];
+                    });
+
+                    for (var j = 1; j < tokens.Length; j += 2)
+                        if (tokens[j].Contains(bab))
+                            return true;
+                }
+            }
+
+            return false;
+
+            static IEnumerable<string> EnumerateAbaPatterns(string token)
+            {
+                for (var i = 0; i < token.Length - 2; i++)
+                    if (token[i] == token[i + 2] && token[i + 1] != token[i])
+                        yield return token.Substring(i, 3);
+            }
+        }
+    }
+}
