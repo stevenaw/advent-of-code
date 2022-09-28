c83e6417afe7c34795d114e4c77931c151482458
stevenaw <sweerdenburg@gmail.com>
2022-09-28 11:27:29 -0400

    do it branchless

--- AdventOfCode.Y2017/Dec03/Puzzle1.cs
+++ AdventOfCode.Y2017/Dec03/Puzzle1.cs
@@ -1,6 +1,4 @@
-﻿using System.Numerics;
-
-namespace AdventOfCode.Y2017.Dec03
+﻿namespace AdventOfCode.Y2017.Dec03
 {
     internal class Puzzle1 : AdventPuzzle
     {
@@ -17,14 +15,14 @@ namespace AdventOfCode.Y2017.Dec03
             var bandMaxValue = (int)Math.Pow(width, 2);
 
             // Convert input to cartesian coordinates (don't need exact)
-            // The "long axis difference" is merely floor(width / 2), or more simply '(width-1) >> 1'
+            // The "long axis difference" is merely floor(width / 2), or more simply '(width-1) >> 1' since we know width is odd
             var distanceFromOriginA = (width - 1) >> 1;
 
             // "short axis diff" is the distance from a midway point on the edge
             // do this by 'normalizing' it to be just before the max value by jumping 'width' around the outside
-            var step = width - 1;
-            while ((bandMaxValue - input) >= width)
-                input += step;
+            // Pre-calculate the number of jumps based on the total distance
+            var jumps = (bandMaxValue - input) / (width - 1);
+            input += jumps * (width - 1);
 
             var midway = (bandMaxValue - distanceFromOriginA);
             var distanceFromOriginB = Math.Abs(input - midway);
