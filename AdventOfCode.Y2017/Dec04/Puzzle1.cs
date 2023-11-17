f2cb5a3632e9069a4494f19047ac66c8084bdb1a
stevenaw <sweerdenburg@gmail.com>
2023-11-17 13:01:17 -0500

    2017 4.1

---
+++ AdventOfCode.Y2017/Dec04/Puzzle1.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode.Y2017.Dec04
+{
+    internal class Puzzle1 : AdventPuzzle
+    {
+        protected override long Solve(IEnumerable<string> lines)
+        {
+            var validCount = 0L;
+
+            foreach(var line in lines)
+            {
+                var tokens = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                var distinctCount = tokens.Distinct(StringComparer.OrdinalIgnoreCase).Count();
+
+                if (distinctCount == tokens.Length)
+                {
+                    validCount++;
+                }
+            }
+
+            return validCount;
+        }
+    }
+}
