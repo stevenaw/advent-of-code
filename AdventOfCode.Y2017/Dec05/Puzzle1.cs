d2879b9b18704e881a3cf4631ac702317df6b5d3
stevenaw <sweerdenburg@gmail.com>
2023-11-20 19:43:06 -0500

    2017 5.1

---
+++ AdventOfCode.Y2017/Dec05/Puzzle1.cs
@@ -0,0 +1,20 @@
+﻿namespace AdventOfCode.Y2017.Dec05
+{
+    internal class Puzzle1 : AdventPuzzle
+    {
+        protected override long Solve(IEnumerable<string> lines)
+        {
+            var jumps = lines.Select(x => int.Parse(x)).ToArray();
+            var i = 0;
+            var stepCount = 0L;
+
+            while (i >= 0 && i < jumps.Length)
+            {
+                i += jumps[i]++;
+                stepCount++;
+            }
+
+            return stepCount;
+        }
+    }
+}
