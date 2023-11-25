66bc97dfe8e7b536fd5b9de202376f1d4e6138be
stevenaw <sweerdenburg@gmail.com>
2023-11-25 13:53:12 -0500

    2017 7.1

---
+++ AdventOfCode.Y2017/Dec07/Puzzle1.cs
@@ -0,0 +1,50 @@
+﻿using System.Runtime.InteropServices;
+
+namespace AdventOfCode.Y2017.Dec07
+{
+    internal class Puzzle1 : AdventPuzzle
+    {
+        protected override long Solve(IEnumerable<string> lines)
+        {
+            var nodes = new Dictionary<string, Node>();
+
+            foreach (var line in lines)
+            {
+                var node = Node.Parse(line);
+                nodes[node.Name] = node;
+            }
+
+            foreach(var nodeName in nodes.Keys)
+            {
+                var found = nodes.Values.Any(x => x.Children.Contains(nodeName));
+
+                if (!found)
+                {
+                    Console.WriteLine($"Root Node: {nodeName}");
+                    return TypeEncoder.EncodeLettersAsLong(nodeName);
+                }
+            }
+
+            return 0;
+        }
+
+        private record Node(string Name, int Weight, List<string> Children)
+        {
+            public static Node Parse(string line)
+            {
+                var items = line.Split(' ');
+
+                var name = items[0];
+                var weight = int.Parse(items[1].AsSpan()[1..^1]);
+                var children = new List<string>();
+
+                for (var i = 3; i < items.Length; i++)
+                {
+                    children.Add(items[i].Trim(','));
+                }
+
+                return new Node(name, weight, children);
+            }
+        }
+    }
+}
