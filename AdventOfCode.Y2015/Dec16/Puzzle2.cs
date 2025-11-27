4d6ba88d0fcffd13059df1afc5cb3219a6dbf6bf
stevenaw <sweerdenburg@gmail.com>
2025-11-26 22:32:38 -0500

    2015.16.2

---
+++ AdventOfCode.Y2015/Dec16/Puzzle2.cs
@@ -0,0 +1,83 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode.Y2015.Dec16
+{
+    internal partial class Puzzle2 : AdventPuzzle
+    {
+        public enum Attributes
+        {
+            children = 0,
+            cats,
+            samoyeds,
+            pomeranians,
+            akitas,
+            vizslas,
+            goldfish,
+            trees,
+            cars,
+            perfumes
+        }
+
+        protected override long Solve(IEnumerable<string> lines)
+        {
+            Span<int> neededAttributes = new int[10];
+
+            neededAttributes[(int)Attributes.children] = 3;
+            neededAttributes[(int)Attributes.cats] = 7;
+            neededAttributes[(int)Attributes.samoyeds] = 2;
+            neededAttributes[(int)Attributes.pomeranians] = 3;
+            neededAttributes[(int)Attributes.akitas] = 0;
+            neededAttributes[(int)Attributes.vizslas] = 0;
+            neededAttributes[(int)Attributes.goldfish] = 5;
+            neededAttributes[(int)Attributes.trees] = 3;
+            neededAttributes[(int)Attributes.cars] = 2;
+            neededAttributes[(int)Attributes.perfumes] = 1;
+
+            foreach(var line in lines)
+            {
+                var parts = ParseLine().Match(line).Groups;
+
+                var sueNumber = int.Parse(parts[1].ValueSpan);
+                var animals = parts[3].Captures;
+                var counts = parts[4].Captures;
+
+                bool match = true;
+                for (int i = 0; i < animals.Count; i++)
+                {
+                    var attribute = Enum.Parse<Attributes>(animals[i].ValueSpan);
+                    var value = int.Parse(counts[i].ValueSpan);
+
+                    if (attribute == Attributes.cats || attribute == Attributes.trees)
+                    {
+                        if (value <= neededAttributes[(int)attribute])
+                        {
+                            match = false;
+                            break;
+                        }
+                    }
+                    else if (attribute == Attributes.pomeranians ||  attribute == Attributes.goldfish)
+                    {
+                        if (value >= neededAttributes[(int)attribute])
+                        {
+                            match = false;
+                            break;
+                        }
+                    }
+                    else if(neededAttributes[(int)attribute] != value)
+                    {
+                        match = false;
+                        break;
+                    }
+                }
+
+                if (match)
+                    return sueNumber;
+            }
+
+            return 0;
+        }
+
+        [GeneratedRegex(@"^Sue (\d+): ((\w+): (\d+)(, )?)+")]
+        private static partial Regex ParseLine();
+    }
+}
