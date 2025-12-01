70904b400f4e532291a6dd43aeef98a62ffe9213
stevenaw <sweerdenburg@gmail.com>
2025-11-30 20:43:05 -0500

    2015 17.2

--- AdventOfCode.Tests/PuzzleTests.cs
+++ AdventOfCode.Tests/PuzzleTests.cs
@@ -47,6 +47,7 @@ namespace AdventOfCode.Tests
             yield return new TestCaseData(2015, 16, 1).SetExpectedResult(40);
             yield return new TestCaseData(2015, 16, 2).SetExpectedResult(241);
             yield return new TestCaseData(2015, 17, 1).SetExpectedResult(1304);
+            yield return new TestCaseData(2015, 17, 2).SetExpectedResult(18);
 
             yield return new TestCaseData(2016, 1, 1).SetExpectedResult(301);
             yield return new TestCaseData(2016, 1, 2).SetExpectedResult(130);
---
+++ AdventOfCode.Y2015/Dec17/Puzzle2.cs
@@ -0,0 +1,50 @@
+﻿namespace AdventOfCode.Y2015.Dec17
+{
+    internal partial class Puzzle2 : AdventPuzzle
+    {
+        const int Total = 150;
+
+        protected override long Solve(IEnumerable<string> lines)
+        {
+            var sizes = lines.Select(int.Parse).ToArray();
+
+            var combos = EnumerateCombos(sizes).ToList();
+
+            var minLength = combos.Min(x => x.Length);
+            var count = combos.Where(x => x.Length == minLength).Count();
+
+            return count;
+        }
+
+        private static IEnumerable<int[]> EnumerateCombos(int[] items)
+        {
+            Array.Sort(items);
+
+            return EnumerateCombos(items, new Stack<int>(items.Length));
+
+            static IEnumerable<int[]> EnumerateCombos(int[] items, Stack<int> combo)
+            {
+                var workingSet = new Stack<int>(items);
+
+                while (workingSet.Count > 0)
+                {
+                    var next = workingSet.Pop();
+                    combo.Push(next);
+                    var sum = combo.Sum();
+
+                    if (sum == Total)
+                    {
+                        yield return combo.ToArray();
+                    }
+                    else if (sum < Total)
+                    {
+                        foreach (var item in EnumerateCombos([.. workingSet], combo))
+                            yield return item;
+                    }
+
+                    combo.Pop();
+                }
+            }
+        }
+    }
+}
