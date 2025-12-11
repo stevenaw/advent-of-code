99f5ecace86e6201f11b7dcf65748b5a95796b83
stevenaw <sweerdenburg@gmail.com>
2025-12-11 12:57:23 -0500

    2015.20.1

---
+++ AdventOfCode.Y2015/Dec20/Puzzle1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AdventOfCode.Y2015.Dec20
+{
+    internal partial class Puzzle1 : AdventPuzzle
+    {
+        protected override long Solve(IEnumerable<string> lines)
+        {
+            var target = long.Parse(lines.First());
+
+            for (var i = 1; i < target / 10; i++)
+            {
+                var gifts = FactorizationHelpers.GetFactors(i).Select(x => x * 10).Sum();
+                if (gifts >= target)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ AdventOfCode/FactorizationHelpers.cs
@@ -0,0 +1,27 @@
+﻿namespace AdventOfCode
+{
+    public static class FactorizationHelpers
+    {
+        public static List<int> GetFactors(int num)
+        {
+            List<int> factors = new List<int>();
+
+            // Iterate up to the square root of the number for efficiency
+            for (int i = 1; i <= Math.Sqrt(num); i++)
+            {
+                if (num % i == 0)
+                {
+                    factors.Add(i);
+                    // If the factors are not the same (e.g., if num is not a perfect square)
+                    // add the pair (num / i) as well
+                    if (i != num / i)
+                    {
+                        factors.Add(num / i);
+                    }
+                }
+            }
+
+            return factors;
+        }
+    }
+}
