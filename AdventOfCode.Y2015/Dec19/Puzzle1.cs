649bb729a7102d20ad4058aecae0a3e900080b8d
stevenaw <sweerdenburg@gmail.com>
2025-12-11 10:59:27 -0500

    refactor to make it easier for the next puzzle

--- AdventOfCode.Y2015/Dec19/Puzzle1.cs
+++ AdventOfCode.Y2015/Dec19/Puzzle1.cs
@@ -1,40 +1,10 @@
-﻿using System;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-namespace AdventOfCode.Y2015.Dec19
+﻿namespace AdventOfCode.Y2015.Dec19
 {
     internal partial class Puzzle1 : AdventPuzzle
     {
         protected override long Solve(IEnumerable<string> lines)
         {
-            Dictionary<string, List<string>> transforms = new Dictionary<string, List<string>>();
-            var transformSpanLookup = transforms.GetAlternateLookup<ReadOnlySpan<char>>();
-
-            var enumerator = lines.GetEnumerator();
-
-            // Build mapping
-            while (enumerator.MoveNext() && !string.IsNullOrEmpty(enumerator.Current))
-            {
-                var currentSpan = enumerator.Current.AsSpan();
-
-                var idx = currentSpan.IndexOf(" => ");
-                var needle = currentSpan.Slice(0, idx);
-                var replacement = currentSpan.Slice(idx + " => ".Length);
-
-                if (!transformSpanLookup.TryGetValue(needle, out var replacements))
-                {
-                    transformSpanLookup[needle] = replacements = new List<string>();
-                }
-
-                replacements.Add(replacement.ToString());
-            }
-
-            string haystack = string.Empty;
-            if (enumerator.MoveNext())
-            {
-                haystack = enumerator.Current;
-            }
+            var (transforms, haystack) = PuzzleInput.Parse(lines);
 
             var distinctCount = EnumerateReplacements(haystack, transforms).Distinct().Count();
 
---
+++ AdventOfCode.Y2015/Dec19/PuzzleInput.cs
@@ -0,0 +1,40 @@
+﻿namespace AdventOfCode.Y2015.Dec19
+{
+    internal record struct PuzzleInput(Dictionary<string, List<string>> transforms, string haystack)
+    {
+        internal static PuzzleInput Parse(IEnumerable<string> lines)
+        {
+            const string TransformDelimiter = " => ";
+
+            Dictionary<string, List<string>> transforms = new Dictionary<string, List<string>>();
+            var transformSpanLookup = transforms.GetAlternateLookup<ReadOnlySpan<char>>();
+
+            var enumerator = lines.GetEnumerator();
+
+            // Build mapping
+            while (enumerator.MoveNext() && !string.IsNullOrEmpty(enumerator.Current))
+            {
+                var currentSpan = enumerator.Current.AsSpan();
+
+                var idx = currentSpan.IndexOf(TransformDelimiter, StringComparison.Ordinal);
+                var needle = currentSpan.Slice(0, idx);
+                var replacement = currentSpan.Slice(idx + TransformDelimiter.Length);
+
+                if (!transformSpanLookup.TryGetValue(needle, out var replacements))
+                {
+                    transformSpanLookup[needle] = replacements = new List<string>();
+                }
+
+                replacements.Add(replacement.ToString());
+            }
+
+            string haystack = string.Empty;
+            if (enumerator.MoveNext())
+            {
+                haystack = enumerator.Current;
+            }
+
+            return new PuzzleInput(transforms, haystack);
+        }
+    }
+}
