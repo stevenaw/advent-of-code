79570467f4b7ff9aa7606814b1b4af67c66e0ba0
stevenaw <sweerdenburg@gmail.com>
2026-05-11 12:13:21 -0400

    2018.4.1

---
+++ AdventOfCode.Y2018/Dec04/Puzzle1.cs
@@ -0,0 +1,75 @@
+﻿namespace AdventOfCode.Y2018.Dec04
+{
+    internal class Puzzle1 : AdventPuzzle
+    {
+        protected override long Solve(IEnumerable<string> lines)
+        {
+            var data = lines.ToArray();
+            Array.Sort(data);
+
+            var sleepWindows = new Dictionary<int, int[]>();
+            var sleepDurations = new Dictionary<int, int>();
+            var currentGuard = 0;
+            var sleepStart = 0;
+
+            foreach (var line in data)
+            {
+                var x = GuardAction.Parse(line);
+
+                if (x.desc.StartsWith("Guard #", StringComparison.Ordinal))
+                {
+                    var x2 = x.desc.Slice(7);
+                    var end = x2.IndexOf(' ');
+
+                    currentGuard = int.Parse(x2.Slice(0, end));
+                    sleepStart = 0;
+
+                    if (!sleepDurations.ContainsKey(currentGuard))
+                    {
+                        sleepWindows[currentGuard] = new int[60];
+                        sleepDurations[currentGuard] = 0;
+                    }
+                }
+                else if (x.desc.Equals("falls asleep", StringComparison.Ordinal))
+                {
+                    sleepStart = x.ts.Minute;
+                }
+                else if (x.desc.Equals("wakes up", StringComparison.Ordinal))
+                {
+                    var duration = x.ts.Minute - sleepStart;
+
+                    sleepDurations[currentGuard] += duration;
+
+                    for (var i = sleepStart; i < x.ts.Minute; i++)
+                    {
+                        sleepWindows[currentGuard][i]++;
+                    }
+                }
+            }
+
+            var guardAsleepMost = sleepDurations.GetKeyOfMaxValue();
+            var minuteAsleepMost = sleepWindows[guardAsleepMost].IndexOfMaxValue();
+
+            return guardAsleepMost * minuteAsleepMost;
+        }
+
+        
+
+        internal ref struct GuardAction(DateTime ts, ReadOnlySpan<char> desc)
+        {
+            public readonly DateTime ts = ts;
+            public readonly ReadOnlySpan<char> desc = desc;
+
+            internal static GuardAction Parse(string line)
+            {
+                var l = line.AsSpan(1); // trim leading '['
+
+                var idx = l.IndexOf(']');
+                var ts = l.Slice(0, idx);
+                var desc = l.Slice(idx + 2);
+
+                return new GuardAction(DateTime.Parse(ts), desc);
+            }
+        }
+    }
+}
--- AdventOfCode/EnumerableExtensions.cs
+++ AdventOfCode/EnumerableExtensions.cs
@@ -6,5 +6,42 @@
         {
             return ((IEnumerable<T>)source).GetEnumerator();
         }
+
+        public static TKey GetKeyOfMaxValue<TKey, TValue>(this Dictionary<TKey, TValue> dict)
+            where TValue : notnull, IComparable<TValue>
+            where TKey : notnull
+        {
+            TKey keyOfMaxValue = default!;
+            TValue maxValue = default!;
+
+            foreach (var kvp in dict)
+            {
+                if (kvp.Value.CompareTo(maxValue) > 0)
+                {
+                    maxValue = kvp.Value;
+                    keyOfMaxValue = kvp.Key;
+                }
+            }
+
+            return keyOfMaxValue;
+        }
+
+        public static int IndexOfMaxValue<T>(this T[] data)
+            where T : notnull, IComparable<T>
+        {
+            int indexOfMaxValue = 0;
+            T maxValue = default!;
+
+            for (var i = 0; i < data.Length; i++)
+            {
+                if (data[i].CompareTo(maxValue) > 0)
+                {
+                    maxValue = data[i];
+                    indexOfMaxValue = i;
+                }
+            }
+
+            return indexOfMaxValue;
+        }
     }
 }
